ee943e74cc5f24bdeb89bfeb92280cc60f1276fd
Matthieu Beauché <mathieu.beauche@gmail.com>
2014-06-02 11:28:20 -0400

    - jQuery autocomplete: autoFocus: true (sélectionne le premier résultat de la liste) - Caching `$(this)` in a `$this` variable - changing constant select_url to simple var

--- app/assets/javascripts/searchify/searchify.js
+++ app/assets/javascripts/searchify/searchify.js
@@ -1,34 +1,38 @@
-(function( $ ) {
-    $.fn.searchify = function() {
-        return this.each(function() {
-            $(this).autocomplete({
-                source: $(this).data("search-url"),
-                delay: $(this).data('delay') || 500,
+(function ($) {
+    $.fn.searchify = function () {
+        return this.each(function () {
+            var $this = $(this);
+            $this.autocomplete({
+                source: $this.data('search-url'),
+                delay: $this.data('delay') || 500,
+                autoFocus: $this.data('autofocus') || true,
                 appendTo: 'body',
                 select: function (event, ui) {
-                    if (select_url = $(this).data("select-url")) {
+                    var select_url = $this.data("select-url");
+                    if (select_url) {
                         for (element in ui.item)
                             select_url = select_url.replace('\(' + element + '\)', ui.item[element]);
 
                         Turbolinks.visit(select_url);
                     } else {
-                        $(this).prev().val(ui.item.id);
-                        $(this).data('value', ui.item.id)
-                        $(this).blur();
-                        $(this).focus();
+                        $this.prev().val(ui.item.id);
+                        $this.data('value', ui.item.id);
+                        $this.blur();
+                        $this.focus();
                     }
                 }
             });
 
-            $(this).change( function (event, ui) {
-                if ( $(this).prev().val() == '' || $(this).prev().val() != $(this).data('value') ) {
-                    $(this).val('');
-                    $(this).prev().val('');
+            $this.change(function (event, ui) {
+                if ($this.prev().val() == '' || $this.prev().val() != $this.data('value')) {
+                    $this.val('');
+                    $this.prev().val('');
                 }
             });
-            $(this).focus( function (event, ui) {
-                $(this).data('value', '');
+
+            $this.focus(function (event, ui) {
+                $this.data('value', '');
             });
         });
     };
-})( jQuery );
+})(jQuery);
